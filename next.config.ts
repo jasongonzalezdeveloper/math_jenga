d2a7e95626f90286f13edc6d0ccbe71f2460ae0b
Jason Gonzalez <jason.gonzalez.developer@gmail.com>
2026-02-23 16:46:45 -0600

    assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
 
   // If deploying to a repo (not username.github.io), uncomment and set your repo name:
   // basePath: '/math_jenga',
-  // assetPrefix: '/math_jenga',
+  assetPrefix: '/math_jenga',
 };
 
 export default nextConfig;
